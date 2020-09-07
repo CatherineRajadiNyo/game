7846b867f891ecaa436b4a4db38bcacaa00bf77d
Catherine Rajadi <catherine.rajadi@gmail.com>
2020-09-07 17:22:45 +0800

    Add formatter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const minWidth = 50;
 const maxWidth = 150;
 
 function App() {
+    const [create, setCreate] = useState(false);
+    const [join, setJoin] = useState(false);
+
     const { innerWidth: windowWidth, innerHeight: windowHeight } = window;
     const numRows = 6;
     const numCols = Math.floor(windowWidth / minWidth);
@@ -28,26 +31,40 @@ function App() {
         setGrid(newGrid)
     }
 
+    const createRoom = () => {
+        setCreate(true)
+        setJoin(true)
+    }
+
+    const joinRoom = () => {
+        setCreate(true)
+        setJoin(true)
+    }
+
     return (
-        <div style={{
-            display: "grid",
-            gridTemplateColumns: `repeat(${numCols}, ${width}px)`,
-            margin: '0 auto'
-        }}>
-            {_.map(grid, (rows, i) => {
-                return _.map(rows, (cols, k) => (
-                    <div
-                        key={`${i}-${k}`}
-                        onClick={() => changeColor(i, k)}
-                        style={{
-                            width: width,
-                            height: width,
-                            backgroundColor: grid[i][k] ? "pink" : undefined,
-                            border: '1px solid black'
-                        }}>{`${i}-${k}`}</div>
-                ))}
-            )}
-        </div>
+        <>
+            <button disabled={create} onClick={()=>createRoom()}>create</button>
+            <button disabled={join} onClick={()=>joinRoom()}>join</button>
+            <div style={{
+                display: "grid",
+                gridTemplateColumns: `repeat(${numCols}, ${width}px)`,
+                margin: '0 auto'
+            }}>
+                {_.map(grid, (rows, i) => {
+                    return _.map(rows, (cols, k) => (
+                        <div
+                            key={`${i}-${k}`}
+                            onClick={() => changeColor(i, k)}
+                            style={{
+                                width: width,
+                                height: width,
+                                backgroundColor: grid[i][k] ? "pink" : undefined,
+                                border: '1px solid black'
+                            }}>{`${i}-${k}`}</div>
+                    ))}
+                )}
+            </div>
+        </>
     );
 }
 
