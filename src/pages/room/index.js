3b43ca920a9a92f52ee6e590631c17e83cf330f0
Catherine Rajadi <catherine.rajadi@gmail.com>
2020-09-09 22:23:53 +0800

    If user not found, redirect back to home

--- src/pages/room/index.js
+++ src/pages/room/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { useCookies } from 'react-cookie'
 import _ from 'lodash'
 import { useRoom } from '@hooks'
@@ -6,13 +7,19 @@ import Grid from './grid'
 import GameStats from './game-stats'
 
 const Room = () => {
+  const history = useHistory()
   const [cookies, setCookie] = useCookies(['user'])
+
   const { isFetching, room } = useRoom()
 
   if (isFetching) return <h1>Loading Room...</h1>
   if (!room) return <h1>Room Not Found...</h1>
 
   const { player1Id, player2Id } = room
+
+  if (_.isEmpty(cookies.user)) {
+    history.push('/')
+  }
 
   let playerNumber
   if (player1Id == cookies.user) playerNumber = 1
